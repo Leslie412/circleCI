0e1c1c7a3b7b4fa37f65198ca7293e9df265ad2f
leslie <wenwu.zhang@rakuten.com>
2020-09-22 18:19:35 +0900

    first commit

---
+++ src/main/java/com/example/circleCi/controller/CircleCIController.java
@@ -0,0 +1,19 @@
+package com.example.circleCi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CircleCIController {
+    @GetMapping("/")
+    public String hello() {
+        return "index";
+    }
+
+    @GetMapping("/pressbtn")
+    public String message(Model model){
+        model.addAttribute("message", "You've pressed button!");
+        return "index";
+    }
+}
