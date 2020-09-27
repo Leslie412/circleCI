3bbe592b18d4f9639f9645b9f2202bdd9bff7731
leslie <wenwu.zhang@rakuten.com>
2020-09-27 19:40:07 +0900

    answer for optional question

--- src/main/java/com/example/circleCi/controller/CircleCIController.java
+++ src/main/java/com/example/circleCi/controller/CircleCIController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CircleCIController {
+
     @GetMapping("/")
     public String hello() {
         return "index";
